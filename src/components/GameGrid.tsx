45e9e2e05cbd0181a6ea6a76502bef76fc7f0def
Steffen-Svenningsen <stef7625@gmail.com>
2025-02-10 12:29:24 +0100

    Split into custom hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,26 +1,7 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-    id: number;
-    name: string;
-}
-
-interface GameResponse {
-    count: number;
-    results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState<String>('');
-
-    useEffect(() => {
-        apiClient.get<GameResponse>('/games')
-            .then(res => setGames(res.data.results))
-            .catch((err) => setError(err.message))
-    }, [])
-
+    const { games, error} = useGames();
     return (
         <div>
             {error && <p>{error}</p>}
