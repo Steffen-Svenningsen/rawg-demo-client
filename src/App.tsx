4b439b9cb95171878d0dc47fe37945b1913b08a7
Steffen-Svenningsen <stef7625@gmail.com>
2025-02-10 10:49:28 +0100

    Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react"
+
 function App() {
   return (
     <div className="App">
-      App
+      <Grid
+        templateAreas={{
+          base: `"header header"
+                  "main main"`,
+          lg: `"header header"
+                "aside main"`
+        }}
+
+      >
+        <GridItem pl='2' bg='orange.300' area={'header'}>
+          Header
+        </GridItem>
+        <Show above="lg">
+          <GridItem pl='2' bg='pink.300' area={'aside'}>
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem pl='2' bg='green.300' area={'main'}>
+          Main
+        </GridItem>
+      </Grid>
     </div>
   )
 }
